3982e913f3bc7486f520539116e0e98e2f501626
txaa2019 <txaa2019@POM-ITB-DGX01.campus.pomona.edu>
2022-12-06 23:01:01 -0800

    sentiment analysis training, loading, analyzing complete, making all analysis into big main method

--- ScrapedCSVAnalysis/GeneralAnalysis.py
+++ ScrapedCSVAnalysis/GeneralAnalysis.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import importlib
 import AdjectiveAnalysis as aa
+import BertProcesses as bp
 
+importlib.reload(bp)
 importlib.reload(aa)
 
 fsize = 15
@@ -26,6 +28,11 @@ plt.rcParams['ytick.major.size'] = major
 plt.rcParams['ytick.minor.size'] = minor
 # %%
 dir = "C:\\Users\\tyxia\\OneDrive - Pomona College\\Pomona\\pai\\P-YUM\\ChannelCsvs\\clean\\"
+# %%
+pred_sentences = ['This was an awesome movie. I watch it twice my time watching this beautiful movie if I have known it was this good',
+                  'One of the worst movies of all time. I cannot believe I wasted two hours of my life for this movie','I am doing ok']
+
+
 # %%
 def time_text_to_sec(text_in):
     time_ints = [int(i) for i in text_in.split() if i.isdigit()]
@@ -96,7 +103,53 @@ def fetch_wpm(df_in, outname = '', save= False):
     if save == True:
         plt.savefig(outname + '.png')
     plt.show()
+
+# %%
+def fetch_sponsor_rag(string_in):
+    words = string_in.split(' ')
     
+    for i in range(len(words)):
+        if 'sponsor' in words[i].lower():
+            return words[i-2]
+    return ''
+
+
+# %%
+def partition_transcript(text):
+    part_size = 40
+    word_arr = text.split(' ')
+    text_chunk_arr = []
+    for i in range(0, len(word_arr), part_size):
+        text_chunk_arr.append(' '.join(word_arr[i:i+part_size]))
+    return text_chunk_arr
+    
+
+
+# %%
+def bert_sentiment(df_in, transcript_bool = True):
+    if transcript_bool:
+        list_trans = df_in['transcript'].tolist()
+        trans_sent = []
+        for transcript in list_trans:
+            try:
+                partitioned_transcript = partition_transcript(transcript)
+                sentiments = bp.make_pred(partitioned_transcript)
+                trans_sent.append(np.mean(sentiments))
+            except:
+                trans_sent.append(None)
+        return pd.Series(trans_sent, index = ['Transcript Sentiments']) 
+    else:
+        vid_names = df_in['Video Name'].tolist()
+        preds = bp.make_pred(vid_names)
+        
+        return pd.Series(preds, index = ['Video Name Sentiments'])
+
+
+# %%
+def perform_analysis(df_in):
+    
+
+
 # %%
 ramsay_df = pd.read_csv(dir + "GordonRamsayVideos.csv")
 fetch_wpm(ramsay_df)

714773c3686558a97e112937a6d05c9d644568ac
tyxiang0530 <tyxiang0530@gmail.com>
2022-12-06 20:40:42 -0800

    cleaning nbs for raw scraping outputs, batch analysis for cleaned videos

--- ScrapedCSVAnalysis/GeneralAnalysis.py
+++ ScrapedCSVAnalysis/GeneralAnalysis.py
@@ -25,7 +25,8 @@ plt.rcParams['xtick.minor.size'] = minor
 plt.rcParams['ytick.major.size'] = major
 plt.rcParams['ytick.minor.size'] = minor
 # %%
-dir = "C:\\Users\\tyxia\\OneDrive - Pomona College\\Pomona\\pai\\P-YUM\\YouTubeScraping\\"
+dir = "C:\\Users\\tyxia\\OneDrive - Pomona College\\Pomona\\pai\\P-YUM\\ChannelCsvs\\clean\\"
+# %%
 def time_text_to_sec(text_in):
     time_ints = [int(i) for i in text_in.split() if i.isdigit()]
     return 60 * (time_ints[0]) + time_ints[-1]
@@ -65,10 +66,40 @@ def return_timing_info(df_in, outname='', save = False):
 # %%
 def fetch_transcript_list(df_in):
     return list(df_in['transcript'])
+# %%
+def fetch_wpm(df_in, outname = '', save= False):
+    df_in = df_in[df_in['transcript'].apply(lambda x: isinstance(x,str))]
+    df_in["length (m)"] = df_in["length"].apply(time_text_to_min)
+    df_in["num_words"] = df_in['transcript'].str.split().apply(len).value_counts()
+    df_in["wpm"] = df_in["num_words"] / df_in["length (m)"]
+    ax = df_in.hist(column='wpm', bins=25, grid=False, figsize=(12,8), color='#86bf91', zorder=2, rwidth=0.9)
+    ax = ax[0]
+    
+    for x in ax:
+        # Despine
+        x.spines['right'].set_visible(False)
+        x.spines['top'].set_visible(False)
+        x.spines['left'].set_visible(False)
 
+        # Switch off ticks
+        x.tick_params(axis="both", which="both", bottom="off", top="off", labelbottom="on", left="off", right="off", labelleft="on")
+        # Draw horizontal axis lines
+        vals = x.get_yticks()
+        for tick in vals:
+            x.axhline(y=tick, linestyle='dashed', alpha=0.4, color='#eeeeee', zorder=1)
+        # Remove title
+        x.set_title("")
+        # Set x-axis label
+        x.set_xlabel("Words per minute", labelpad=20, weight='bold', size=12)
+        # Set y-axis label
+        x.set_ylabel("Frequency", labelpad=20, weight='bold', size=12)
+    if save == True:
+        plt.savefig(outname + '.png')
+    plt.show()
+    
 # %%
 ramsay_df = pd.read_csv(dir + "GordonRamsayVideos.csv")
-text = fetch_transcript_list(ramsay_df)
+fetch_wpm(ramsay_df)
 # %%
 aa.gen_wordcloud(text)
 # %%
